57c19165c8792854140ce72a877843ed33ec7029
JD <jeshan.dangol@gmail.com>
2018-01-18 23:19:05 +0900

    header added to first two columns of pixel indicating height and width as resolutions

--- stego.py
+++ stego.py
@@ -17,8 +17,9 @@ for each pixel(x,y) in carrier_file:
 	r = 7bits of carrier + 1 bit of passenger
 
 Specifications
-1	Can take only png files as input. 
-2	Fills in the  ( length x breadth )	number of pixels starting from top-left corner. 
+1	Can take only png files as input, outputs file in png format.
+2	Ensures carrier img is large enough to accommodate passenger img before hiding.
+3	Fills in the  ( length x breadth )	number of pixels starting from top-left corner. 
 	From top to bottom, then next column of pixels on the right
 
 Problems:
@@ -43,18 +44,18 @@ def dec_to_bin(x, length = 8):	#default length is 8 bits
 		ans = "0" + ans
 	return (ans)		#returns in the form of list, need to cast using int before use as integer. e.g. int(dec_to_bin(10))[:7]
 
-def edit_pixel(pic,carrier,output):
+def edit_pixel(passenger,carrier,output):
 	# reading data to be encoded
 
-	level = 4	#number of LSB positions to use up for encoding
+	level = 2	#number of LSB positions to use up for encoding
 	r_vals = []
 	g_vals = []
 	b_vals = []
 
 	#this whole data might be acquired using getpixels() function
-	for x in xrange(pic.size[0]):
-		for y in xrange(pic.size[1]):
-			r, g, b, _ = pic.getpixel((x, y))
+	for x in xrange(passenger.size[0]):
+		for y in xrange(passenger.size[1]):
+			r, g, b, _ = passenger.getpixel((x, y))
 			#if (r % 8 == 1 or g % 8 == 1 or b % 8 == 1):
 			#print(r,g,b)
 			#r,g,b = (255,255,0)
@@ -74,11 +75,6 @@ def edit_pixel(pic,carrier,output):
 			g_vals.append(g_bin)
 			b_vals.append(b_bin)
 
-	# for x in xrange(0,500):		#find the occurence of first red value of pixel which is not "0"
-	# 	print r_vals[x]
-	# 	if(r_vals[x] != "0"):
-	# 		break
-
 	#return 			""""""""""""""""""""""""""""""""""""""""""""""""
 
 	last_index = len(r_vals) - 1
@@ -88,7 +84,7 @@ def edit_pixel(pic,carrier,output):
 
 	pixel_cursor = 0
 	i = 0
-	for x in xrange(carrier.size[0]):
+	for x in xrange(2,carrier.size[0]): 	#start hiding from column 2 (3rd column)
 		for y in xrange(carrier.size[1]):
 			r, g, b, _ = carrier.getpixel((x, y))
 
@@ -114,7 +110,7 @@ def edit_pixel(pic,carrier,output):
 				print "at", (x,y), (dec_to_bin(r),dec_to_bin(g),dec_to_bin(b)), "->", (r_cr_bin,g_cr_bin,b_cr_bin)#, "->", (r,g,b) #
 
 			elif(pixel_cursor == (last_index + 1)):
-				result_x, result_y = (x,y)
+				result_x, result_y = (x,y)	#for showing information at last
 
 			# else:
 			# 	print ("at", (x,y), "writing unchanged data")
@@ -135,9 +131,46 @@ def edit_pixel(pic,carrier,output):
 	print "Encoding finished at cursor", (result_x, result_y) 
 	return
 
-def read_pixel(pic,carrier,output):
-	#new_img = Image.new('RGB', pic.size)
-	edit_pixel(pic,carrier,output)
+def write_header():
+	#write the header information into the carrier
+	#store resolution
+	psngr_height = dec_to_bin(passenger.size[0], 16)		#return height as 1 bit binary values
+	psngr_width = dec_to_bin(passenger.size[1], 16)
+	print psngr_height,psngr_width
+
+	#write_header() : to encode height and width in first two columns
+	print "encoding psngr_height"
+	x = 0
+	i = 0	#bit_position
+	for y in xrange(carrier.size[1]):	
+		r, g, b, _ = carrier.getpixel((x, y))
+		
+		if(i < 16):	#16 digits to be written
+			r = dec_to_bin(r)[:7] + psngr_height[i:(i+1)]
+			r = int(r,2)
+			print r
+			i = i + 1
+
+		output.putpixel((x,y), (r,g,b))	
+
+	print "encoding psngr_width"
+	x = 1
+	i = 0
+	for y in xrange(carrier.size[1]):
+		r, g, b, _ = carrier.getpixel((x, y))
+		
+		if(i < 16):	#16 digits to be written
+			r = dec_to_bin(r)[:7] + psngr_width[i:(i+1)]
+			r = int(r,2)
+			print r
+			i = i + 1
+
+		output.putpixel((x,y), (r,g,b))	
+
+def hide(passenger,carrier,output):
+	#new_img = Image.new('RGB', passenger.size)
+	write_header()
+	#edit_pixel(passenger,carrier,output)
 	output.save("output.png", "PNG", optimize=True)
 	
 	# hist, bins = np.histogram(output,256,[0,256])
@@ -146,9 +179,25 @@ def read_pixel(pic,carrier,output):
 	# plt.show()
 	return
 
-def extract_image(composite_image, output):
+def read_header():
+	dimension = ["",""]
+	for x in range(2):	#from first 2 columns
+		for y in range(16):
+			r, _ , _ = composite_image.getpixel((x, y))
+			#print r
+			r = dec_to_bin(r,16)
+			dimension[x] = dimension[x] +  r[15:16]
+		print
+	print "dimensions are", int(dimension[0],2), int(dimension[1],2)
+	return int(dimension[0],2), int(dimension[1],2)
 
-	pass
+def show(composite_image, output):
+	for x in xrange(2,composite_image.size[0]): 	#start reading from column 2 (3rd column)
+		for y in xrange(composite_image.size[1]):
+			r, g, b = composite_image.getpixel((x, y))
+			#extract bit from each pixel until the l*b pixel is reached, taking (level) number of bits from LSB
+		print
+	return
 
 def usage():
 	print "Usage:\n # stego.py hide file.png carrier.png output.png \n # stego.py show composite_image.png output.png"
@@ -176,34 +225,35 @@ if __name__ == '__main__':
 
 	if(sys.argv[1] == "hide" and len(sys.argv) == 5):
 		print "hiding"
-		pic = Image.open(sys.argv[2])
-		print(pic)
+		passenger = Image.open(sys.argv[2])
+		print(passenger)
 		carrier = Image.open(sys.argv[3])
 		print(carrier)
 		#need to encode the number of pixels edited too, for decoding
 		output = Image.new('RGB', carrier.size)
-		
-		#write the header information into the carrier
-		#store resolution
-		psngr_height = dec_to_bin(pic.size[0], 16)
-		psngr_breadth = dec_to_bin(pic.size[1], 16)
-		print psngr_height,psngr_breadth
-		exit(-1)
-
-		#start image processing
-		read_pixel(pic, carrier, output)
-		#print(pic.histogram)
 
 		#create a system to ensure the carrier image is large enough to hold the header, and the passenger data
-		# if():
-		# 	print "Carrier image not large enough to hide passenger image."
-		# 	exit(1)
+		size_required = passenger.size[0] * passenger.size[1]
+		size_available = (carrier.size[0] - 2) * carrier.size[1]
+		if(carrier.size[1] < 16 or size_available < size_required):
+			print "ERROR: Carrier image not large enough to hide passenger image."
+			exit(1)
+
+		#start image processing
+		hide(passenger, carrier, output)
+
+		#print(passenger.histogram)
 		
 		
 	elif (sys.argv[1] == "show" and len(sys.argv) == 4):
 		print "showing"
 		composite_image = Image.open(sys.argv[2])
-		#output = Image.new('RGB', carrier.size)
+
+		#retrieve header, if unsuccessful decode not possible
+		passenger_height, passenger_width = read_header()		
+
+		output = Image.new('RGB', (passenger_height,passenger_width))	#SIZE IS NOT APPROPRIATE
+		show(composite_image, output)
 
 	else:
 		usage()
